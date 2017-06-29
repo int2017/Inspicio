a49d5ac851b3d2c6ba742b79fc0313065f5a0d36
Gytis Gecevicius <Gytis.Gecevicius@nonlinear.com>
2017-06-29 13:53:03 +0100

    Merge commit

--- Inspicio/Controllers/HomeController.cs
+++ Inspicio/Controllers/HomeController.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
+
 namespace Inspicio.Controllers
 {
-    [Authorize]
+    
     public class HomeController : Controller
     {
         public IActionResult Index()
