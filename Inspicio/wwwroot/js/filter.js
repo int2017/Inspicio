fd4a6b60564b569646de467003b82849af11b28d
Gytis Gecevicius <Gytis.Gecevicius@nonlinear.com>
2017-06-30 16:08:24 +0100

    Fixed Dropzone, added filtering to projects.
    Filters now work in the dashboard. Uses classes.
    Dropzone only accepts 1 image, makes it as the background.

---
+++ Inspicio/wwwroot/js/filter.js
@@ -0,0 +1,22 @@
+﻿$("#filters :checkbox").click(function () {
+    
+    var n = 0;
+    if ($(this).attr("id") === "all") {
+        $(".dash-project").show();
+        $("#all:checkbox").prop("checked", true);
+    }
+    else {
+        $("#all:checkbox").prop("checked", false);
+        n = 0;
+        $(".dash-project").hide();
+        $("#filters :checkbox:checked").each(function () {
+            $("." + $(this).val()).show();
+            n++;
+        });
+        if (n == 0) {
+            $("#filters :checkbox").prop("checked", false);
+            $(".dash-project").show();
+            $("#all:checkbox").prop("checked", true);
+        }
+    }
+});
