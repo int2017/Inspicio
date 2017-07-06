4dc7da58ebed011bea35cb4370dd23bb3611178a
kerrylewis <kerry.lewis0097@gmail.com>
2017-07-06 10:30:08 +0100

    Clear text area
    Text area is now cleared when a comment is submitted.

--- Inspicio/wwwroot/js/ajaxComments.js
+++ Inspicio/wwwroot/js/ajaxComments.js
@@ -5,7 +5,11 @@
         var data = {
                 "ImageId": $("#ImageId").val(),
                 "Message": $("#comment-textarea").val()
+                   
         }
+
+         
+
         $.ajax(
             {
                 type: "POST", //HTTP POST Method  
@@ -16,8 +20,9 @@
                 
                 success: function () {
                     $("#comment-section").load(window.location.href + " #comment-section");
-                    
+                    $("#comment-textarea").val('');
                 }
-            });
+
+              });
 
     }); 
