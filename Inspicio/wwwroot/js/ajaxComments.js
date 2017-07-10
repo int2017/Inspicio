dc61177e337a1b46f8dbf9979db3203ac39f5212
SnappiestJack <Snappiestjack@users.noreply.github.com>
2017-07-10 13:23:33 +0100

    Revert "Marker popups add comments, redesigned view page"

--- Inspicio/wwwroot/js/ajaxComments.js
+++ Inspicio/wwwroot/js/ajaxComments.js
@@ -1,8 +1,20 @@
+﻿
+// Allows you to submit text using enter button
+$('#comment-textarea').keypress(function (e) {
+    if (e.which == 13) {
+        $('#submit-comment').click();
+        // prevent duplicate submission
+        return false; 
+    }
+});
 
-function commentClick (uniqID) {
+    //function will be called on button click having id btnsave
+$("#submit-comment").click(function () {
+
         var data = {
                 "ImageId": $("#ImageId").val(),
-                "Message": $(".popup-textarea").val()          
+                "Message": $("#comment-textarea").val()
+                   
         }
 
          
@@ -16,41 +28,10 @@ function commentClick (uniqID) {
                 data: JSON.stringify(data),
                 
                 success: function () {
-                    createCommentRow("user", $(".popup-textarea").val(), uniqID);
-                    $("#comment-section").load(window.location.href + " #comment-section > * ");
-                    $("#comment-textarea").val("");
-
+                    $("#comment-section").load(window.location.href + " #comment-section");
+                    $("#comment-textarea").val('');
                 }
 
               });
 
-}; 
-
-function commentClickMain() {
-    var data = {
-        "ImageId": $("#ImageId").val(),
-        "Message": $("#comment-textarea").val()
-    }
-    $.ajax(
-        {
-            type: "POST", //HTTP POST Method  
-            url: "../Comment", // Controller/View  
-            contentType: "application/json;",
-            dataType: "text",
-            data: JSON.stringify(data),
-
-            success: function () {
-                $("#comment-section").load(window.location.href + " #comment-section > * ");
-                $("#comment-textarea").val("");
-            }
-        });
-
-}; 
-
-$('#comment-textarea').keypress(function (e) {
-    if (e.which == 13) {
-        $('#submit-comment').click();
-        // prevent duplicate submission
-        return false; 
-    }
-});
+    }); 
