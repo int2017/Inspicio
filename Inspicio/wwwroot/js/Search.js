0adbcec605af0e40bd2b25c047e29190fa040a80
kerrylewis <kerry.lewis0097@gmail.com>
2017-07-14 12:08:27 +0100

    Js function to search through table and hide irrelevant rows
    JS has been added to replace the original search bar in imageController. It loops through the rows in the table and hides irrelevant data. If user wants to see all results, they simply need to empty the search bar. Some formatting changed and old search bar features removed.

---
+++ Inspicio/wwwroot/js/Search.js
@@ -0,0 +1,25 @@
+﻿function myFunction() {
+
+  var input, filter, table, tr, td, 
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("table");
+  tr = table.getElementsByTagName("tr");
+
+  // Loops through table rows, and hides ones that don't match the search query
+  for (int i = 0; i < tr.length; i++) {
+      tdTitle = tr[i].getElementsByTagName("td")[1];
+      tdDescription = tr[i].getElementsByTagName("td")[2];
+      if (tdTitle) {
+          if (tdTitle.innerHTML.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+          }
+          else if (tdDescription.innerHTML.toUpperCase().indexOf(filter) > -1){
+              tr[i].style.display = "";
+           }
+        else {
+            tr[i].style.display = "none";
+        }
+    }
+  }
+}
