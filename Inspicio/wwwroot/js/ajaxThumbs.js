7cf937a831476937516e39e84c52bec359de33c0
Gytis Gecevicius <Gytis.Gecevicius@nonlinear.com>
2017-07-19 11:59:16 +0100

    The thumb which the user last-clicked is now disabled.
    Used ajaxThumbs.js to insert a method, that either uses an ajax call or the front end information to update the thumb buttons. Ajax is used when loading the page, front-end information is used when users change their vote.

--- Inspicio/wwwroot/js/ajaxThumbs.js
+++ Inspicio/wwwroot/js/ajaxThumbs.js
@@ -28,7 +28,8 @@ $(".thumb").click(function () {
     
 });
 
-function updateThumbs( chosenState, button ) {
+function updateThumbs(chosenState, button) {
+    
     var text = "#" + button + " .rating";
     var image = {
         "ImageID": $("#ImageId").val(),
@@ -47,6 +48,49 @@ function updateThumbs( chosenState, button ) {
                 $("#thumbs-middle > span").load(window.location.href + " " + "#thumbs-middle > span");
                 $("#thumbs-down > span").load(window.location.href + " " + "#thumbs-down > span");
                 $(".reviewer-row").load(window.location.href + " .reviewer-row >*");
+                disableThumb(true, chosenState);
             }
         });
+}
+//On page load, it gets the last review of the user from the database, on thumb click,it uses front-end information to update the thumbs
+$(document).ready(function () {
+    disableThumb(false);
+})
+function disableThumb(ifClick, chosenState) {
+    $("#thumb-container button.btn").removeClass("disabled");
+    if (ifClick) {
+        if (chosenState == 0) {
+            $("#thumbs-up").addClass("disabled");
+        }
+        else if (chosenState == 1) {
+            $("#thumbs-middle").addClass("disabled");
+        }
+        if (chosenState == 2) {
+            $("#thumbs-down").addClass("disabled");
+        }
+    }
+    else{
+    var id = $("#ImageId").val();
+    $.ajax(
+        {
+            type: "GET", //HTTP GET Method  
+            url: "../GetRating", // Controller/View  
+            contentType: "application/json;",
+            dataType: "json",
+            data: {
+                id: id
+            },
+            success: function (data) {
+                if (data == 0) {
+                    $("#thumbs-up").addClass("disabled");
+                }
+               else if (data == 1) {
+                    $("#thumbs-middle").addClass("disabled");
+                }
+                if (data == 2) {
+                    $("#thumbs-down").addClass("disabled");
+                }
+            }
+        });
+    }
 }
