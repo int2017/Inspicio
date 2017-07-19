02e12c4ab52a60e0d2635a033b3ee5fa0d5cb350
kerrylewis <kerry.lewis0097@gmail.com>
2017-07-19 12:17:08 +0100

    Search by Creator
    The search feature now allows users to search by creator.

--- Inspicio/wwwroot/js/Search.js
+++ Inspicio/wwwroot/js/Search.js
@@ -8,15 +8,19 @@
 
   // Loops through table rows, and hides ones that don't match the search query
   for (i = 0; i < tr.length; i++) {
-      tdTitle = tr[i].getElementsByTagName("td")[1];
-      tdDescription = tr[i].getElementsByTagName("td")[2];
+      tdCreator = tr[i].getElementsByTagName("td")[1];
+      tdTitle = tr[i].getElementsByTagName("td")[2];
+      tdDescription = tr[i].getElementsByTagName("td")[3];
       if (tdTitle) {
           if (tdTitle.innerHTML.toUpperCase().indexOf(filter) > -1) {
               tr[i].style.display = "";
           }
           else if (tdDescription.innerHTML.toUpperCase().indexOf(filter) > -1){
               tr[i].style.display = "";
-           }
+          }
+          else if (tdCreator.innerHTML.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+          }
         else {
             tr[i].style.display = "none";
         }
