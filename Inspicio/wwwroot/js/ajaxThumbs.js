0a2826d3df5211ad85fa4f817ae7e82f377fdeb7
Gytis Gecevicius <Gytis.Gecevicius@nonlinear.com>
2017-07-19 12:31:13 +0100

    Fixed issue where thumb click listeners would still fire even if the button was disabled.
    Had to turn off the listener for disabled buttons.

--- Inspicio/wwwroot/js/ajaxThumbs.js
+++ Inspicio/wwwroot/js/ajaxThumbs.js
@@ -3,30 +3,31 @@
     NeedsWork: 1,
     Rejected: 2
 };
-
-$(".thumb").click(function () {
-
-    var button = $(this).attr("id");
-    var className = $("#" + button + " i").attr("class");
-    $("#" + button + " i").removeClass(className);
-    if (className.toLowerCase().indexOf("-o") > 0) {
-        className = className.replace("-o", "");
-    }
-    else {
-        className = className.slice(0, 12) + "-o" + className.slice(12);
-    }
-    if (button === "thumbs-up") {
-        updateThumbs(thumbEnum.Approved, button);
-    }
-    else if (button === "thumbs-down") {
-        updateThumbs(thumbEnum.Rejected, button);
-    }
-    else if (button === "thumbs-middle") {
-        updateThumbs(thumbEnum.NeedsWork, button);
-    }
-    $("#" + button + " i").addClass(className);
+function addThumbListener() {
     
-});
+        var button = $(this).attr("id");
+        var className = $("#" + button + " i").attr("class");
+        $("#" + button + " i").removeClass(className);
+        if (className.toLowerCase().indexOf("-o") > 0) {
+            className = className.replace("-o", "");
+        }
+        else {
+            className = className.slice(0, 12) + "-o" + className.slice(12);
+        }
+        alert(className);
+        if (button === "thumbs-up") {
+            updateThumbs(thumbEnum.Approved, button);
+        }
+        else if (button === "thumbs-down") {
+            updateThumbs(thumbEnum.Rejected, button);
+        }
+        else if (button === "thumbs-middle") {
+            updateThumbs(thumbEnum.NeedsWork, button);
+        }
+        $("#" + button + " i").addClass(className);
+
+   
+}
 
 function updateThumbs(chosenState, button) {
     
@@ -55,19 +56,11 @@ function updateThumbs(chosenState, button) {
 //On page load, it gets the last review of the user from the database, on thumb click,it uses front-end information to update the thumbs
 $(document).ready(function () {
     disableThumb(false);
+    $(".thumb").click(addThumbListener);
 })
 function disableThumb(ifClick, chosenState) {
-    $("#thumb-container button.btn").removeClass("disabled");
     if (ifClick) {
-        if (chosenState == 0) {
-            $("#thumbs-up").addClass("disabled");
-        }
-        else if (chosenState == 1) {
-            $("#thumbs-middle").addClass("disabled");
-        }
-        if (chosenState == 2) {
-            $("#thumbs-down").addClass("disabled");
-        }
+        changeState(chosenState);
     }
     else{
     var id = $("#ImageId").val();
@@ -81,16 +74,25 @@ function disableThumb(ifClick, chosenState) {
                 id: id
             },
             success: function (data) {
-                if (data == 0) {
-                    $("#thumbs-up").addClass("disabled");
-                }
-               else if (data == 1) {
-                    $("#thumbs-middle").addClass("disabled");
-                }
-                if (data == 2) {
-                    $("#thumbs-down").addClass("disabled");
-                }
+                changeState(data);
             }
         });
+    }
+}
+function changeState(state) {
+    $("#thumb-container button.btn").removeClass("disabled");
+    $(".thumb").off();
+    $(".thumb").click(addThumbListener);
+    if (state == 0) {
+        $("#thumbs-up").addClass("disabled");
+        $("#thumbs-up").off();
+    }
+    else if (state == 1) {
+        $("#thumbs-middle").addClass("disabled");
+        $("#thumbs-middle").off();
+    }
+    if (state == 2) {
+        $("#thumbs-down").addClass("disabled");
+        $("#thumbs-down").off();
     }
 }
