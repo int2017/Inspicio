a8b865e94a044140093649d4e4169a64201604c3
kerrylewis <kerry.lewis0097@gmail.com>
2017-07-19 14:13:24 +0100

    Description Column removed from viewing all (inc searching)
    Description column has been removed as some descriptions may be very long and ruin the appearance. The information will also be redundant when we start uploading more than one image. The ability to search by description has also been removed,

--- Inspicio/wwwroot/js/Search.js
+++ Inspicio/wwwroot/js/Search.js
@@ -10,12 +10,8 @@
         for (i = 0; i < tr.length; i++) {
             tdCreator = tr[i].getElementsByTagName("td")[1];
             tdTitle = tr[i].getElementsByTagName("td")[2];
-           tdDescription = tr[i].getElementsByTagName("td")[3];
             if (tdTitle) {
                 if (tdTitle.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                }
-                else if (tdDescription.innerHTML.toUpperCase().indexOf(filter) > -1) {
                     tr[i].style.display = "";
                 }
                 else if (tdCreator.innerHTML.toUpperCase().indexOf(filter) > -1) {
