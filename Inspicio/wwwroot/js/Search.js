d39e51113634f97ddca1f5a20784f03c9983708e
kerrylewis <kerry.lewis0097@gmail.com>
2017-07-19 14:07:51 +0100

    Prevented page refreshing on enter
    The page doesn't refresh if enter is pressed. To do this, the listener has been removed from the index page and the js has been changed in Search.js to include a function for @keyup. The form containing the search box has been removed to prevent refreshing.

--- Inspicio/wwwroot/js/Search.js
+++ Inspicio/wwwroot/js/Search.js
@@ -1,29 +1,30 @@
-﻿function searchFunction() {
+﻿$("#myInput").on("keyup",function () {
+    
+        var input, filter, table, tr, td, i;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("table");
+        tr = table.getElementsByTagName("tr");
 
-    var input, filter, table, tr, td, i; 
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("table");
-  tr = table.getElementsByTagName("tr");
-
-  // Loops through table rows, and hides ones that don't match the search query
-  for (i = 0; i < tr.length; i++) {
-      tdCreator = tr[i].getElementsByTagName("td")[1];
-      tdTitle = tr[i].getElementsByTagName("td")[2];
-      tdDescription = tr[i].getElementsByTagName("td")[3];
-      if (tdTitle) {
-          if (tdTitle.innerHTML.toUpperCase().indexOf(filter) > -1) {
-              tr[i].style.display = "";
-          }
-          else if (tdDescription.innerHTML.toUpperCase().indexOf(filter) > -1){
-              tr[i].style.display = "";
-          }
-          else if (tdCreator.innerHTML.toUpperCase().indexOf(filter) > -1) {
-              tr[i].style.display = "";
-          }
-        else {
-            tr[i].style.display = "none";
+        // Loops through table rows, and hides ones that don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            tdCreator = tr[i].getElementsByTagName("td")[1];
+            tdTitle = tr[i].getElementsByTagName("td")[2];
+           tdDescription = tr[i].getElementsByTagName("td")[3];
+            if (tdTitle) {
+                if (tdTitle.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                }
+                else if (tdDescription.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                }
+                else if (tdCreator.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                }
+                else {
+                    tr[i].style.display = "none";
+                }
+            }
         }
-    }
-  }
-}
+    
+});
