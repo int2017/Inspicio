66c7a32b4a9b978ae10c58f725946dbb6cbc80bd
Gytis Gecevicius <Gytis.Gecevicius@nonlinear.com>
2017-07-20 14:43:11 +0100

    Updated homepage feature images, homepage feature examples work almost as a slide now.

---
+++ Inspicio/wwwroot/js/homepageScripts.js
@@ -0,0 +1,17 @@
+﻿var section = 2;
+
+function switchSection() {
+    setTimeout(function () {
+        $("[id^='section'").hide();
+        $("#section" + section).show();
+        section++;
+    }, 2000)
+    
+}
+
+$(".switch-section").click(function () {
+    if ($("#chatbot-check").is(":checked")) {
+        switchSection();
+    }
+    else alert("Select people for your review first!")
+})
