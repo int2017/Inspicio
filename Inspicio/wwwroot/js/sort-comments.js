ab03df419c92f944dee13c1e28d98ba67fa06424
Gytis Gecevicius <Gytis.Gecevicius@nonlinear.com>
2017-07-24 15:21:44 +0100

    Ability to sort comments by date added
    Either select the date you wish to view in the select dropdown,or click on the date header you want to view.
    
    Fixed the way uploaded images are displayed in the Homepage ( they used to be cropped before, now they are displayed in the correct size)
    
    Fixed the background selector being too wide. Now it only fits the image

---
+++ Inspicio/wwwroot/js/sort-comments.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    var dates = [];
+    $(".date-header").each(function () {
+        var dateVal = $(this).children().data("value");
+        if ($.inArray(dateVal, dates)) {
+            var option = document.createElement('option');
+            $(option).html(dateVal).val(dateVal);
+            $(option).appendTo("#comment-date");
+            dates.push(dateVal.toLowerCase());
+        }
+    }).click(function () {
+        var date = $(this).children().data("value");
+        $("#comment-date").val(date).change();
+    })
+
+    $("#comment-date").change(function () {
+        if ($(this).val() == "all") {
+            $(".date-header").removeClass("active");
+            $(".comment").slideDown(300);
+        }
+        else {
+            $(".comment").slideUp(300);
+            var value = $(this).val().toLowerCase();
+
+            $(".date-header").each(function () {
+                var dateVal = $(this).children().data("value");
+                if (dateVal.toLowerCase() == value) {
+                    $(this).addClass("active");
+
+                }
+            })
+            $("." + value).delay(300).slideDown(400);
+        }
+
+    })
+});
