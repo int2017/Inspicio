4a5f9636f573fd77382851b5badf6ea59152928e
Gytis Gecevicius <Gytis.Gecevicius@nonlinear.com>
2017-07-31 09:34:44 +0100

    Initial chatbot functionality

---
+++ Inspicio/wwwroot/js/chatBot.js
@@ -0,0 +1,9 @@
+﻿function activateChatBot() {
+    var width = $("#homeImageMap").width();
+    var height = $("#homeImageMap").height();
+    var loc = new L.latLng(width * Math.random(), height * Math.random());
+    setTimeout(function () {
+        createMarker(loc);
+    })
+
+}
