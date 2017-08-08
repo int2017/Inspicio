7c7dccd67876c2487fbdd036d048eb131f438476
kerrylewis <kerry.lewis0097@gmail.com>
2017-08-08 12:02:01 +0100

    Heartbeat replaced with checkFocus
    CheckFocus has been added to see if a user is currently on an inspicio tab every 30 seconds and change the flags in DB based on this. If they are on the tab, they don't receive notifications. If they are not on the tab, notifications are enabled for that user.

---
+++ Inspicio/wwwroot/js/ajaxCheckFocus.js
@@ -0,0 +1,30 @@
+﻿function checkFocus(id, flag) {
+
+
+    $.ajax({
+        url: "/Account/CompareTime",
+        type: 'POST',
+        data: {
+            id: id,
+            flag: flag
+        },
+        success: function (response) {
+
+            if (!document.hasFocus()) {
+                setTimeout(function ()
+                {
+                    checkFocus(id, true)
+                }
+                    , 30000);
+
+            }
+            else {
+                setTimeout(function () {
+                    checkFocus(id, false)
+                }
+                    , 30000);
+            }
+        }
+    });
+
+};
