85130ec2bfa92e270df7eeee65cf60cd02b902f6
kerrylewis <kerry.lewis0097@gmail.com>
2017-08-09 16:34:46 +0100

    Design changes made to login/register
    Changes made to login/register page to make it more obvious which page the user is currently on.

--- Inspicio/wwwroot/js/login_register.js
+++ Inspicio/wwwroot/js/login_register.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
     $("#registersection").click(function () {
         var w = document.getElementById('loginformmain');
         $(w).hide();
+        $("#loginsection").css('backgroundColor', '	#D3D3D3');
+        $("#registersection").css('backgroundColor', '#000000');
         var x = document.getElementById('registerform');
         $(x).show();
     })
@@ -10,6 +12,8 @@ $(document).ready(function () {
     $("#loginsection").click(function () {
         var z = document.getElementById('registerform');
         $(z).hide();
+        $("#registersection").css('backgroundColor', '#D3D3D3');
+        $("#loginsection").css('backgroundColor', '#000000');
         var y = document.getElementById('loginformmain');
         $(y).show();
        
