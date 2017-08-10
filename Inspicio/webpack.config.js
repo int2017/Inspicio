d97cd19e81b9cf006334726d3d8cba22c6470aca
Gytis Gecevicius <Gytis.Gecevicius@nonlinear.com>
2017-08-10 11:47:23 +0100

    Fixed a few minor issues in JS and .csproj files
    Removed unused .js files from Inspicio.csproj
    Left the "if" statement blank in createDropzone.js
    Removed the modules.export block with image_view.js stuff in it, also deleted the files associated.

--- Inspicio/webpack.config.js
+++ Inspicio/webpack.config.js
@@ -1,19 +1,6 @@
 ﻿var path = require('path');
 
 module.exports = {
-	entry: {
-		"images_view": './Scripts/images_view.js'	
-	},
-	output: {
-		filename: "[name].bundle.js",
-		path: path.resolve(__dirname, 'wwwroot/dist/')
-	},
-	resolve: {
-		extensions: [".js"]
-	}
-};
-
-module.exports = {
     entry: {
         "ajaxScreens": './Scripts/ajaxScreens.js',
     },
